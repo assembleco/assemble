e72a443360ea3e0252434c5c6dd65e652537cbe5
Grayson Wright <wright.grayson@gmail.com>
2016-11-06 08:36:06 -0800

    Add syntax highlighting for flows and schemas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def highlight_json(json)
+    CodeRay.
+      scan(json, :json).
+      div.
+      html_safe
+  end
+
+  def highlight_flow(flow)
+    language = {
+      ruby: :ruby,
+      node: :javascript,
+      python2: :python,
+    }.with_indifferent_access[flow.environment]
+
+    CodeRay.scan(flow.body, language).div.html_safe
+  end
 end
