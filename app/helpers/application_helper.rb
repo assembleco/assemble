9eb3432815dd4291120850b4e0c9aef153845cd3
Calliope <thecalliopecrashed@gmail.com>
2016-11-06 20:36:06 -0800

    Add help text to Flow form and show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,6 @@
 module ApplicationHelper
   def highlight_json(json)
-    CodeRay.
-      scan(json, :json).
-      div.
-      html_safe
+    CodeRay.scan(json, :json).div.html_safe
   end
 
   def highlight_flow(flow)
@@ -14,5 +11,9 @@ module ApplicationHelper
     }.with_indifferent_access.fetch(flow.environment)
 
     CodeRay.scan(flow.body, language).div.html_safe
+  end
+
+  def prettify(json)
+    JSON.pretty_generate(JSON.parse(json))
   end
 end
