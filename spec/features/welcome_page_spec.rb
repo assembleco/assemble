6c18116b07806236ee0869ef54a0411810f1b2b6
Calliope <thecalliopecrashed@gmail.com>
2017-01-09 11:31:53 -0800

    Create a welcome page
    * Add tests for sign in and sign up
    * Only show welcome page to non-signed-in users
    * Redirect signed-in users to `/explore` (`flows#index`)

---
+++ spec/features/welcome_page_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.feature "Welcome page" do
+  scenario "user sees a friendly greeting & description of the site" do
+    visit root_path
+
+    expect(page).to have_content t("welcome.index.greeting")
+    expect(page).to have_content t("welcome.index.description.one")
+    expect(page).to have_content t("welcome.index.description.two")
+  end
+
+  scenario "user signs in" do
+    create(:user, username: "foobar", password_digest: "password")
+
+    visit root_path
+    click_on "Sign in"
+    fill_in :username, with: "foobar"
+    fill_in "Password", with: "password"
+    click_button "Sign in"
+
+    expect(page).to have_content t("sessions.create.success", name: "foobar")
+  end
+
+  scenario "user signs up" do
+    visit root_path
+    click_on "Sign up"
+    fill_in "Username", with: "foobar"
+    fill_in "Email", with: "foobar@example.com"
+    fill_in "Password", with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content t("users.create.success", name: "foobar")
+  end
+
+  scenario "signed-in user is redirected to explore page" do
+    sign_in create(:user)
+
+    visit root_path
+
+    expect(current_path).to eq(explore_path)
+  end
+end
