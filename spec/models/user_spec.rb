aa5e5d011bcf799216700e007bbda251011123d2
Grayson Wright <wright.grayson@gmail.com>
2017-01-12 17:56:31 -0800

    Test out building blocks directly on their page
    We don't need a special `sandbox` page to test blocks.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,36 @@ describe User do
   describe "validations" do
     specify "username must contain only alphanumeric characters and underscores"
   end
+
+  describe "#sandbox_feed_for" do
+    it "returns the sandbox feed for a block if it exists" do
+      user = create(:user)
+      block = create(:block)
+      feed = create(:feed)
+      create(:connection, app: user.sandbox_app, source: feed, destination: block)
+
+      sandbox_feed = nil
+
+      expect {
+        sandbox_feed = user.sandbox_feed_for(block)
+      }.not_to change(Feed, :count)
+
+      expect(sandbox_feed).to eq(feed)
+    end
+
+    it "creates and returns a sandbox feed for a block if it does not yet exist" do
+      block = create(:block)
+      user = create(:user)
+
+      feed = nil
+
+      expect {
+        feed = user.sandbox_feed_for(block)
+      }.to change(Feed, :count).by(1)
+
+      connection = feed.connections.first
+      expect(connection.destination).to eq(block)
+      expect(connection.app).to eq(user.sandbox_app)
+    end
+  end
 end
