6d1c97af79be562a82d2bda6f6936d087e33ad76
Grayson Wright <wright.grayson@gmail.com>
2017-01-12 20:24:42 -0800

    Create rudimentary app visualization
    We should expand this to let users create new connections
    between blocks and feeds.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,7 +2,7 @@ class Feed < ApplicationRecord
   has_many :events
   has_many :connections, as: :source
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   def to_param
     name
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,13 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
+  SANDBOX_APP_DESCRIPTION = <<-DESCRIPTION
+  This app is automatically created for you so that you can test out blocks that other people create.
+  DESCRIPTION
+
   def sandbox_app
-    apps.find_or_create_by!(name: "sandbox")
+    apps.find_by(name: "sandbox") ||
+      apps.create!(name: "sandbox", description: SANDBOX_APP_DESCRIPTION)
   end
 
   def sandbox_feed_for(block)
