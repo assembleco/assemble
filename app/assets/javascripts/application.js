1b1900151130c96ea68fd7f05217184a36ecbceb
Grayson Wright <wright.grayson@gmail.com>
2017-01-17 14:28:16 -0800

    Install react-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
 //= require jquery
 //= require jquery_ujs
+//= require react
+//= require react_ujs
 
-//= require_tree .
+//= require_tree ./components
