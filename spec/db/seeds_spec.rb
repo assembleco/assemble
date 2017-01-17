c3c1fd8a6fa73a9584453d81563b10c97059d61e
Grayson Wright <wright.grayson@gmail.com>
2017-01-17 14:18:00 -0800

    Add tests for database seeds

---
+++ spec/db/seeds_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe "db/seeds.rb" do
+  it "deletes existing records" do
+    create(:user, username: "olduser")
+
+    load("db/seeds.rb")
+
+    expect(User.where(username: "olduser")).to be_empty
+  end
+
+  it "creates records" do
+    load("db/seeds.rb")
+
+    expect(User.count).to eq(1)
+    expect(Block.count).to eq(3)
+  end
+end
