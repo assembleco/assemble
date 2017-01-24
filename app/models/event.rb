63c174b3df00c106bb578732ba763f541b67774a
Grayson Wright <wright.grayson@gmail.com>
2017-01-24 11:48:40 -0800

    Create time-based feeds with rufus-scheduler

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,17 @@
 class Event < ApplicationRecord
+  belongs_to :feed
+
+  def save
+    super
+
+    feed.connections.each do |connection|
+      block_run = BlockRun.create!(
+        app: connection.app,
+        block: connection.destination,
+        input: data,
+      )
+
+      block_run.delay.execute
+    end
+  end
 end
---
+++ config/initializers/scheduler.rb
@@ -0,0 +1,38 @@
+require "rufus-scheduler"
+
+schedule = Rufus::Scheduler.singleton
+
+# only schedule when not running from the Ruby on Rails console
+# or from a rake task
+unless defined?(Rails::Console) ||
+  File.split($0).last == "rake" ||
+  Rails.env.test?
+
+  # Every week
+  schedule.cron "0 0 * * 0" do
+    Rails.logger.info "Running weekly job at #{Time.now}"
+    Rails.logger.flush
+    Event.create(feed: Feed.find_by!(name: "weekly"), data: { time: Time.now })
+  end
+
+  # Every day at midnight
+  schedule.cron "0 0 * * *" do
+    Rails.logger.info "Running daily job at #{Time.now}"
+    Rails.logger.flush
+    Event.create(feed: Feed.find_by!(name: "daily"), data: { time: Time.now })
+  end
+
+  # Every hour on the hour
+  schedule.cron "0 * * * *" do
+    Rails.logger.info "Running hourly job at #{Time.now}"
+    Rails.logger.flush
+    Event.create(feed: Feed.find_by!(name: "hourly"), data: { time: Time.now })
+  end
+
+  # Every minute on the minute
+  schedule.cron "* * * * *" do
+    Rails.logger.info "Running minutely job at #{Time.now}"
+    Rails.logger.flush
+    Event.create(feed: Feed.find_by!(name: "minutely"), data: { time: Time.now })
+  end
+end
