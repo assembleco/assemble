0c4e8f180a2c5018cf9cb770209efb5e01695aef
Grayson Wright <wright.grayson@gmail.com>
2017-01-27 20:40:25 -0800

    Add a clearer CTA on explore page

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -9,6 +9,7 @@ require('babel-polyfill');
 global.BlockSandboxForm = require('components/block_sandbox_form.es6').default;
 global.AppCanvas = require('components/app_canvas.es6').default;
 global.SchemaBuilder = require('components/schema_builder.es6').default;
+global.ExploreHero = require('components/explore_hero.es6').default;
 
 global.Form = require("react-jsonschema-form").default;
 global.JSONTree = require("react-json-tree").default;
