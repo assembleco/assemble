ec640980bc454ae97738b237a6c18845c869008e
Calliope <thecalliopecrashed@gmail.com>
2017-02-16 13:19:19 -0800

    Show block run info after triggering in UI

---
+++ webpack/components/run_status.jsx
@@ -0,0 +1,81 @@
+import React from "react"
+import Radium from "radium"
+import $ from "jquery"
+
+import colors from "../styles/colors"
+
+class RunStatus extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.checkBlockStatus = this.checkBlockStatus.bind(this);
+  }
+
+  componentDidMount() {
+    this.checkBlockStatus();
+  }
+
+  render() {
+    if(this.state.status)
+      return (
+        <div
+          style={[RunStatus.styles.status, RunStatus.styles[this.state.status]]}
+          >
+          Run completed – {this.state.status}
+
+          <div>
+            STDOUT:
+            <pre>
+            {this.state.stdout}
+            </pre>
+          </div>
+
+          <div>
+            STDERR:
+            <pre>
+            {this.state.stderr}
+            </pre>
+          </div>
+        </div>
+      );
+    else
+      return(
+        <div
+          style={[RunStatus.styles.status, RunStatus.styles[this.state.status]]}
+          >
+          Run is pending...
+        </div>
+      );
+  }
+
+  checkBlockStatus() {
+    $.get(this.props.url, (response) => {
+      if(response.status === "pending") {
+        setTimeout(this.checkBlockStatus, 500);
+      } else {
+        this.setState(response);
+      }
+    })
+  }
+}
+
+RunStatus.styles = {
+  status: {
+    padding: "0.75rem",
+    marginBottom: "1.5rem",
+  },
+  pending: {
+    backgroundColor: colors.yellow
+  },
+  success: {
+    backgroundColor: colors.green
+  }
+}
+
+RunStatus.propTypes = {
+  url: React.PropTypes.string.isRequired,
+}
+
+export default Radium(RunStatus);
--- webpack/styles/colors.js
+++ webpack/styles/colors.js
@@ -5,20 +5,4 @@ const colors = {
   red: "#fb987f",
 }
 
-const border_size = "4px";
-
-function border_for_app_id(id) {
-  const border_id = (id - 1) % 4 + 1;
-  const border_color = Object.values(colors)[border_id];
-
-  return {
-    border: [
-      border_size,
-      "solid",
-      border_color
-    ].join(" "),
-    borderRadius: border_size,
-  };
-}
-
-export default border_for_app_id;
+export default colors;
