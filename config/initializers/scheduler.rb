0160f7be43bb5804ba974f26326e4a66e9ae9ade
Calliope <thecalliopecrashed@gmail.com>
2017-03-22 19:50:10 -0700

    Remove unused scheduled background jobs

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -8,31 +8,5 @@ unless defined?(Rails::Console) ||
   File.split($0).last == "rake" ||
   Rails.env.test?
 
-  # Every week
-  schedule.cron "0 0 * * 0" do
-    Rails.logger.info "Running weekly job at #{Time.now}"
-    Rails.logger.flush
-    Event.create(feed: Feed.find_by!(name: "Every Week"), data: { time: Time.now })
-  end
-
-  # Every day at midnight
-  schedule.cron "0 0 * * *" do
-    Rails.logger.info "Running daily job at #{Time.now}"
-    Rails.logger.flush
-    Event.create(feed: Feed.find_by!(name: "Every Day"), data: { time: Time.now })
-  end
-
-  # Every hour on the hour
-  schedule.cron "0 * * * *" do
-    Rails.logger.info "Running hourly job at #{Time.now}"
-    Rails.logger.flush
-    Event.create(feed: Feed.find_by!(name: "Every Hour"), data: { time: Time.now })
-  end
-
-  # Every minute on the minute
-  schedule.cron "* * * * *" do
-    Rails.logger.info "Running minutely job at #{Time.now}"
-    Rails.logger.flush
-    Event.create(feed: Feed.find_by!(name: "Every Minute"), data: { time: Time.now })
-  end
+  # Define tasks here - see Rufus::Scheduler docs
 end
