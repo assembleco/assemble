6bc2a4f968b6d8e98e96d8969f58a820ed760fa0
Calliope <thecalliopecrashed@gmail.com>
2017-04-21 16:51:24 -0700

    Write basic test for BlockForm
    See http://airbnb.io/enzyme/docs/guides/jsdom.html

--- test/components/block_Form_test.js
+++ test/components/block_Form_test.js
@@ -1,16 +1,33 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { mount } from "enzyme"
 import { expect } from 'chai';
 
 import BlockForm from "../../webpack/components/block_form"
-import TabNavigation from "../../webpack/components/tab_navigation"
 
 var assert = require("assert");
 
-describe("<BlockForm/>", () => {
-  it("renders a <TabNavigation/>", () => {
-    const wrapper = shallow(<BlockForm />);
+const defaultProps = {
+  block: {},
+  submit: {
+    label: "Submit",
+    method: "POST",
+    url: "/form_endpoint",
+  },
+  title: "Block Form",
+}
 
-    expect(wrapper.find(TabNavigation)).to.have.length(1);
+describe("<BlockForm/>", () => {
+  describe("Build tab", () => {
+    it("displays relevant form fields", () => {
+      const form = mount(<BlockForm {...defaultProps} />);
+
+      expect(form.find("input[type='text']")).to.have.length(3);
+    });
+  });
+
+  describe("Test tab", () => {
+  });
+
+  describe("Publish tab", () => {
   });
 });
--- test/index.js
+++ test/index.js
@@ -1,4 +1,18 @@
-var context = require.context('./', true, /\.js$/);
+var jsdom = require('jsdom').jsdom;
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
+
+var context = require.context('./components', true, /\.js$/);
 
 context.keys().forEach(context);
 module.exports = context;
