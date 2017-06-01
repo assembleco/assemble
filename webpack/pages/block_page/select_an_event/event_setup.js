961455d8c965ee50620ac86276907cf10794ea16
Calliope <thecalliopecrashed@gmail.com>
2017-06-01 17:30:10 -0400

    WIP Continue laying out events

---
+++ webpack/pages/block_page/select_an_event/event_setup.js
@@ -0,0 +1,18 @@
+import React from "react"
+import PropTypes from "prop-types"
+import styled from "styled-components"
+
+import Hint from "components/hint"
+
+class EventSetup extends React.Component {
+  render() {
+    return(
+      <div>
+        TODO: Block setup & configuration,
+        and the information that the event passes to the block.
+      </div>
+    );
+  }
+}
+
+export default EventSetup;
