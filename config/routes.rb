08da3b9be67a4e7480ef0b5b21c7786d93a16951
Grayson Wright <wright.grayson@gmail.com>
2017-06-06 16:13:30 -0400

    Cleanup: Remove old views, controllers, and routes
    We're starting to break out the front-end from the back-end,
    and the views are the tightest point of coupling.
    
    We'll remove all views not necessary for the core work flow,
    and slowly refactor over other views to use an API.

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,9 @@
 
 Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create"
-  delete "/session", to: "sessions#destroy"
-
-  get "/user_info", to: "users#show", as: :user_info
-  get "/about", to: "about#index", as: :about
-
   resource :session, only: [:new, :destroy]
 
-  # User path
-  resources :users, only: [:show], param: :handle
-  resource :user, only: [:edit, :update], as: :profile
-
-  resources :connections, only: [:create]
-  resources :subscriptions, only: [:create]
-
-  resources :defaults, only: [:create]
+  get "/about", to: "about#index", as: :about
 
   # Block paths
   post "/blocks", to: "blocks#create"
@@ -25,7 +13,6 @@ Rails.application.routes.draw do
   delete "/blocks/:handle/:blockname", to: "blocks#destroy"
 
   # Block runs
-  get "/blocks/:handle/:blockname/runs/:block_run_id", to: "block_runs#show", as: :block_run
   post "/blocks/:handle/:blockname/runs", to: "block_runs#create", as: :block_runs
 
   resources :services, only: [:index]
