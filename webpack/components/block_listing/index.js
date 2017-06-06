e5a69139bdc54afed8849bc455319f423dca4d2c
Calliope <thecalliopecrashed@gmail.com>
2017-06-06 18:08:21 -0400

    Remove CSS classnames from React components

--- webpack/components/block_listing/index.js
+++ webpack/components/block_listing/index.js
@@ -6,27 +6,62 @@ import Gravatar from "react-gravatar"
 import Logo from "components/logo"
 
 const BlockListing = (props) => (
-  <a
+  <ListItem
     href={`/blocks/${props.user.handle}/${props.name}`}
-    className="list-item block-listing"
     >
-    <Logo/>
+    <Icon/>
 
     {props.name}
 
-    <div className="author">
-      <Gravatar
+    <Author>
+      <Avatar
         email={props.user.email}
-        className="avatar"
         alt={`Avatar for ${props.user.handle}`}
         />
 
-      <div className="author-info">
+      <AuthorInfo>
         <div>Created by {props.user.handle}</div>
         <div>on {dateFormat(props.created_at, "mmmm d, yyyy")}</div>
-      </div>
-    </div>
-  </a>
+      </AuthorInfo>
+    </Author>
+  </ListItem>
 )
+
+const ListItem = styled.a`
+  background-color: white;
+  border-radius: 4px;
+  color: black;
+  display: inline-block;
+  flex: 1 1 20%;
+  margin: 0.75rem;
+  padding: 1.5rem
+  border: 1px solid #ddd;
+  text-align: center;
+  margin-right: 1.5rem;
+  margin-bottom: 1.5rem;
+  padding: 0.75rem;
+`
+
+const Icon = styled(Logo)`
+  display: block;
+  height: 3em;
+  margin: auto auto 0.75rem;
+`
+
+const AuthorInfo = styled.div`
+  display: inline-block;
+`
+
+const Avatar = styled(Gravatar)`
+  border-radius: 50%;
+  display: inline-block;
+  height: 3em;
+`
+
+const Author = styled.div`
+  color: gray;
+  margin-bottom: 0;
+  margin-top: 0.75rem;
+`
 
 export default BlockListing
