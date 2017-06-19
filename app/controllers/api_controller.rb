05529bdb879c24f601d0e692dbff8d3e91d26d6a
Calliope <thecalliopecrashed@gmail.com>
2017-06-19 16:42:23 -0400

    Don't require a login to use the API
    Fixes #15
    
    ## Problem
    
    The welcome page would not display any data unless someone is signed in,
    because it requires authentication before it responds to any requests.
    
    ## Solution
    
    Remove the `require_login` filter from `ApiController`.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,6 @@
 class ApiController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:query]
+  skip_before_filter :require_login
 
   def query
     result = Schema.execute(
