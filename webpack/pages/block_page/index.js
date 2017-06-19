25f6df39e66839e2ee4d705708a536fff9f60e93
Grayson Wright <wright.grayson@gmail.com>
2017-06-19 18:04:52 -0400

    Move run results to the right side of screen
    Later, we should animate them in to give more space to the results,
    and more context about the navigation.

--- webpack/pages/block_page/index.js
+++ webpack/pages/block_page/index.js
@@ -18,52 +18,73 @@ import Column from "layout/column"
 
 const BlockPage = ({ data }) => (
   data.loading ? <Loading /> : (
-    <div>
-      <Title
-        id={data.block.id}
-        created_at={data.block.created_at}
-        description={data.block.description}
-        editable={data.block.editable}
-        name={data.block.name}
-        user={data.block.author}
-      />
-
-      { data.session
-      ? <BlockUsage
+    <Wrapper>
+      <MainColumn>
+        <Title
+          id={data.block.id}
+          created_at={data.block.created_at}
+          description={data.block.description}
           editable={data.block.editable}
-          initial_input_data={data.block.initial_input_data}
           name={data.block.name}
-          run_block_url={`${window.location.href}/runs.json`}
-          schema={data.block.schema}
           user={data.block.author}
-          user_api_key={data.session.api_key}
         />
-      : <Section>
-          Sign in with GitHub to try out this block.
-        </Section>
-      }
 
-      <BlockSource
-        id={data.block.id}
-        command={data.block.command}
-        editable={data.block.editable}
-        dockerfile={data.block.dockerfile}
-        name={data.block.name}
-        source={data.block.source}
-        source_path={data.block.source_path}
-        user={data.block.author}
-      />
+        { data.session
+        ? <BlockUsage
+            editable={data.block.editable}
+            initial_input_data={data.block.initial_input_data}
+            name={data.block.name}
+            run_block_url={`${window.location.href}/runs.json`}
+            schema={data.block.schema}
+            user={data.block.author}
+            user_api_key={data.session.api_key}
+          />
+        : <Section>
+            Sign in with GitHub to try out this block.
+          </Section>
+        }
 
-      <Subscription
-        {...data.block.subscription}
-        block_id={data.block.id}
-        schema={data.block.schema}
-      />
+        <BlockSource
+          id={data.block.id}
+          command={data.block.command}
+          editable={data.block.editable}
+          dockerfile={data.block.dockerfile}
+          name={data.block.name}
+          source={data.block.source}
+          source_path={data.block.source_path}
+          user={data.block.author}
+        />
 
-      <BlockRuns block_id={data.block.id} />
-    </div>
+        <Subscription
+          {...data.block.subscription}
+          block_id={data.block.id}
+          schema={data.block.schema}
+        />
+      </MainColumn>
+
+      <RightSidebar>
+        <BlockRuns block_id={data.block.id} />
+      </RightSidebar>
+    </Wrapper>
   )
 )
+
+const Wrapper = styled.div`
+  align-items: stretch;
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+`
+
+const MainColumn = styled.div`
+  margin-right: 1.5rem;
+  overflow-x: scroll;
+  padding: 1.5rem;
+`
+
+const RightSidebar = styled.div`
+  flex: 0 0 20rem;
+`
 
 const BlockPageWithData = graphql(BlockPageQuery, { options: {
   variables: { block_id: window.location.pathname.split("/")[2] }
