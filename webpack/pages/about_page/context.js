9783f1253f7de21bd11df684f8882478d392e1e8
Calliope <thecalliopecrashed@gmail.com>
2017-06-20 17:55:43 -0400

    Move About page markup into React components
    This was the last page that is being rendered server-side in Rails,
    instead of in React.
    
    It was holding us up from incorporating react-router into our app.
    Now we're free to set it up.

---
+++ webpack/pages/about_page/context.js
@@ -0,0 +1,76 @@
+import React from "react"
+import styled from "styled-components"
+
+import { Page } from "hedron"
+import scroll_down from "images/about/scroll_down.svg"
+
+import Section from "components/section"
+
+const urls = {
+  docs: "https://www.notion.so/assemble/Assemble-Documentation-006d3d9b69a0409ba8c456b08acc9cf1#e6b3629c7e7e469181a334fad3721c3b",
+  github: "https://github.com/assembleapp/registry",
+  notion: "https://www.notion.so/assemble/Assemble-Community-c133a13e3e2645fa9d64c74d8d245705#d48e767d43584fd197e6e74f55dcb1f7",
+  slack: "http://assemble-slack.herokuapp.com/",
+}
+
+const Context = ({ scrollHint }) => (
+  <Wrapper width="1200px">
+    { scrollHint &&
+      <ScrollHint>
+        <img src={scroll_down} alt="Scroll down for more" />
+      </ScrollHint>
+    }
+
+    <Aside>
+      <p>
+      Hello!
+      </p>
+
+      <p>
+      You're looking at an early take on how our platform
+      can help nontechnical users leverage the power of custom technology.
+      </p>
+
+      <p>
+      These features have not been built yet –
+      this is simply a vision of what we'd like to achieve.
+      We've published more detailed plans and roadmaps&nbsp;
+      <a href={urls.docs}>in our documentation</a>.
+      </p>
+
+      <p>
+      We want to build our tools to be as accessible and inclusive as possible.
+      To do that, it's necessary to give everyone a voice in how the tools are built.
+      We'd love to hear your thoughts and ideas.
+      </p>
+
+      <p>
+      Get in touch with us
+      <a href={urls.docs}>on Slack</a>,
+      <a href={urls.github}>on GitHub</a>,
+      <a href={urls.notion}>on Notion</a>,
+      or just click on the orange circle in the corner of your screen
+      and start typing.
+      </p>
+    </Aside>
+  </Wrapper>
+);
+
+const Wrapper = styled(Page)`
+  padding-top: 4.5rem;
+  display: flex;
+  justify-content: center;
+`
+
+const ScrollHint = styled.div`
+  width: 4.5rem;
+  margin-right: 1.5rem;
+`
+
+const Aside = styled(Section)`
+  width: 50%;
+  margin-bottom: 4.5rem;
+  background-color: #f2d38e;
+`
+
+export default Context;
