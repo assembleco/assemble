eac61731a8024fedefef1930d156eb4d50e2ce17
Grayson Wright <wright.grayson@gmail.com>
2017-06-21 17:19:36 -0400

    Fire off block runs through the GraphQL API

--- spec/models/block_run_spec.rb
+++ spec/models/block_run_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe BlockRun, type: :model do
+  describe "#execute" do
+    it "runs the block using its default environment" do
+      env = create(:environment, command: "echo hello")
+      block = create(:block, environment: env)
+      run = BlockRun.create!(user: create(:user), block: block, input: {})
+
+      run.execute
+
+      expect(run.stdout).to eq("hello\n")
+    end
+  end
+
   describe "#exit_status=" do
     it "sets status to :success when the exit status is zero" do
       block_run = build(:block_run, exit_status: 0)
