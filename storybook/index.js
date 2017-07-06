9a33ad0f364889c30726efdaceda16736cb56bef
Calliope <thecalliopecrashed@gmail.com>
2017-07-06 15:20:54 -0400

    Set up Storybook for component previews

---
+++ storybook/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import Logo from "../webpack/components/logo"
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <button onClick={action('clicked')}>Hello Button</button>
+  ))
+  .add('with no emoji', () => (
+    <button onClick={action('clicked')}>no emojis</button>
+  ))
+  .add('with bold text', () => (
+    <button onClick={action('clicked')}>Some <strong>more</strong> text</button>
+  ))
+  .add('with italic text', () => (
+    <button onClick={action('clicked')}>Some <strong>more</strong> text</button>
+  ))
+
+storiesOf("Logo", module)
+  .add("stationary", () => (
+    <Logo />
+  ))
+  .add("rotating", () => (
+    <Logo continuous />
+  ))
