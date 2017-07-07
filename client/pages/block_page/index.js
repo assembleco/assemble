74aad91120c8fc6bd784dd3053bdf91c609013b6
Calliope <thecalliopecrashed@gmail.com>
2017-07-06 22:46:08 -0400

    Fix not-signed-in bug on block page
    The page required an API key from the current sesison,
    even though we were no longer using it in the UI.

--- client/pages/block_page/index.js
+++ client/pages/block_page/index.js
@@ -36,9 +36,7 @@ const BlockPage = ({ data }) => (
           initial_input_data={data.block.initial_input_data}
           name={data.block.name}
           schema={data.block.schema}
-          session={data.session}
           source={data.block.source}
-          user_api_key={data.session.api_key}
         />
 
         <Subscription
